d56e1fdc20de6da9d14ed7a7cabbf7f812c680bc
Ismael Santos <ismaelsantos0@users.noreply.github.com>
2026-06-29 13:59:31 -0300

    chore: setup capacitor and github actions for android build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.annalicia.app',
+  appName: 'Annalícia',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
