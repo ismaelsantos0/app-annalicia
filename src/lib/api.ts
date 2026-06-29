4f80683d8662b045f48be09f506223a8d5333e0a
Ismael Santos <ismaelsantos0@users.noreply.github.com>
2026-06-29 14:52:26 -0300

    fix: change api url to localhost:8000

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "https://app-annalicia.onrender.com";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const request = async (endpoint: string, options: RequestInit = {}) => {
   const res = await fetch(`${BASE_URL}${endpoint}`, {
